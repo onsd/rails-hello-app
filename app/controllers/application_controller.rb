e0d8df4ff1a3818528764b64e4a975d83597956f
Takamichi Omori <merrytakamerry@gmail.com>
2020-04-02 22:31:04 +0900

    initial commit for toy_app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "¡Hello, World!"
-  end
-
-  def goodbye
-    render html: "Good Bye"
-  end
 end
